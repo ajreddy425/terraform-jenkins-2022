129080863e3e76ae813e0831834cb2b8a5ca54fe
ajreddy425 <82323282+ajreddy425@users.noreply.github.com>
2022-07-02 23:28:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA2PW47EPQIXUZLTXO"
-  secret_key = "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"
+#   access_key = "AKIA2PW47EPQIXUZLTXO"
+#   secret_key = "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"
 }
